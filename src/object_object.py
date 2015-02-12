32868ea819529a897ec246afeafc144f83f4abc1
VVS1864 <simonovsen@gmail.com>
2015-02-13 01:21:34 +0500

    two commit

--- src/object_object.py
+++
@@ -1,9 +0,0 @@
-
-
-class Root_object:
-    def from_AL(self, AL, content, list_prop):
-        self.conf_dict = {}
-        for i in AL[content]:
-            if i in list_prop:
-                self.conf_dict[i] = AL[content][i]
-        #self.conf_dict = {prop:value for (prop, value) in AL[content].items() if prop in list_prop}
